d8b76e658f33a5ec0c3b5b3b124b22fbc0615054
ldelapena <leah.d.delapena@gmail.com>
2020-10-15 01:03:57 -0700

    Fix rendering of names to be more consistent regardless of length

--- frontend/components/show/events/attendees/attendee_list_item.jsx
+++ frontend/components/show/events/attendees/attendee_list_item.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const AttendeeListItem = ({ attendee }) => {
+    let name = attendee.name.split(' ')
+
     return (
-        <div className="attendee-list-item">
-            <div className="attendee-photo-container">
-                <div className="attendee-photo"></div>
-            </div>
-            <li key={`attendee-${attendee.id}`} className="attendee-name">
-                {attendee.name}
-            </li>
+      <div className="attendee-list-item">
+        <div className="attendee-photo-container">
+          <div className="attendee-photo"></div>
         </div>
-    )
+        <li key={`attendee-${attendee.id}`} className="attendee-name">
+          <p>{name[0]}</p>
+          <p>{name[1]}</p>
+        </li>
+      </div>
+    );
 }
 
 export default AttendeeListItem;
